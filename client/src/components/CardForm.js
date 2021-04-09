9adfb1d57271c17e1c1449fb00749ef8068d9d9e
Gerald Addo-Tetteh <geraldadt@outlook.com>
2021-04-09 13:41:35 +0000

    completed card payment form and functions && addes new image assets

--- client/src/components/CardForm.js
+++ client/src/components/CardForm.js
@@ -4,67 +4,213 @@
   Card Form for CheckoutPage
 */
 
-const CardForm = ({
-  cardNumberRef,
-  cardNumber,
-  setCardNumber,
-  setCardHolder,
-  cardHolder,
-  expirationDateRef,
-  expirationDate,
-  setExpirationDate,
-  cvc,
-  cvcRef,
-  setCvc,
-}) => {
+import { useEffect, useRef, useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import Payment from "payment";
+import FormSubmitButton from "./FormSubmitButton";
+import Loader from "react-loader-spinner";
+import { SnackbarContext } from "../providers/Store";
+
+import visaImage from "../assets/visa.png";
+import mastercardImage from "../assets/mastercard.png";
+import discoverImage from "../assets/discover.png";
+import amexImage from "../assets/american-express.png";
+
+const CardForm = () => {
+  const history = useHistory();
+  const [, setShowSnackbar, , setSnackbarText] = useContext(SnackbarContext);
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardHolder, setCardHolder] = useState("");
+  const [expirationDate, setExpirationDate] = useState("");
+  const [cvc, setCvc] = useState("");
+  const [highlightCard, setHighlightCard] = useState("");
+  const [showLoading, setShowLoading] = useState(false);
+
+  const cardNumberRef = useRef();
+  const cardHolderRef = useRef();
+  const expirationDateRef = useRef();
+  const cvcRef = useRef();
+
+  const cardTypes = {
+    visa: "Visa",
+    mastercard: "Mastercard",
+    discover: "Discover",
+    amex: "American Express",
+  };
+
+  useEffect(() => {
+    Payment.formatCardNumber(cardNumberRef.current);
+    Payment.formatCardCVC(cvcRef.current);
+    Payment.formatCardExpiry(expirationDateRef.current);
+  }, []);
+
+  const handleValidateInput = () => {
+    let error = false;
+    if (!Payment.fns.validateCardNumber(cardNumber)) {
+      cardNumberRef.current.classList.add("error-border");
+      error = true;
+    } else {
+      cardNumberRef.current.classList.remove("error-border");
+    }
+    if (!Payment.fns.validateCardCVC(cvc)) {
+      cvcRef.current.classList.add("error-border");
+      error = true;
+    } else {
+      cvcRef.current.classList.remove("error-border");
+    }
+    if (!Payment.fns.validateCardExpiry(expirationDate)) {
+      expirationDateRef.current.classList.add("error-border");
+      error = true;
+    } else {
+      expirationDateRef.current.classList.remove("error-border");
+    }
+    if (cardHolder.length < 1) {
+      cardHolderRef.current.classList.add("error-border");
+      error = true;
+    } else {
+      cardHolderRef.current.classList.remove("error-border");
+    }
+    return error;
+  };
+  const handleBuildUrl = () => {
+    const orderId = (Math.random() * 1000).toFixed(0);
+    const closedCardNumber = cardNumber.split(" ").join("");
+    const closedCardExpiry = expirationDate.split(" ").join("");
+    return `https://www.paybox.com.co/pay?amount=20.99&currency=GHS&mode=Card&card_type=${cardTypes[highlightCard]}&card_name=${cardHolder}&card_number=${closedCardNumber}&card_expiry=${closedCardExpiry}&card_cvc=${cvc}&payload={}&order_id=PB_${orderId}`;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const error = handleValidateInput();
+    if (error) {
+      setSnackbarText("Invalid Inputs");
+      setShowSnackbar(true);
+      return;
+    }
+    setShowLoading(true);
+    fetch(handleBuildUrl(), {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_USER_TOKEN}`,
+      },
+    })
+      .then((res) => {
+        console.log(res.status);
+        if (res.ok) {
+          return res.text();
+        }
+        throw new Error("Could not complete transaction");
+      })
+      .then((data) => {
+        setShowLoading(false);
+        setSnackbarText("Transaction Complete");
+        setShowSnackbar(true);
+        history.push("/");
+      })
+      .catch((err) => {
+        setShowLoading(false);
+        setSnackbarText(err.message);
+        setShowSnackbar(true);
+      });
+  };
+
   return (
-    <div className="card-form">
-      <label htmlFor="cardNumber">Card Number</label>
-      <input
-        ref={cardNumberRef}
-        type="text"
-        name="cardNumber"
-        id="cardNumber"
-        value={cardNumber}
-        placeholder="xxxx xxxx xxxx xxxx"
-        onChange={(e) => setCardNumber(e.target.value)}
-      />
-      <label htmlFor="cardHolder">Card Holder</label>
-      <input
-        type="text"
-        name="cardHolder"
-        id="cardHolder"
-        value={cardHolder}
-        placeholder="John Deo Shearer"
-        onChange={(e) => setCardHolder(e.target.value)}
-      />
-      <div className="input-horizontal">
-        <div className="input-horizontal--item">
-          <label htmlFor="expirationDate">Exp. Date</label>
-          <input
-            ref={expirationDateRef}
-            className="horizontal"
-            name="expirationDate"
-            id="expirationDate"
-            value={expirationDate}
-            placeholder="MM / YY"
-            onChange={(e) => setExpirationDate(e.target.value)}
-          />
+    <form className="checkout-page-form" method="POST" onSubmit={handleSubmit}>
+      <div className="card-form">
+        <label htmlFor="cardNumber">Card Number</label>
+        <input
+          ref={cardNumberRef}
+          type="text"
+          name="cardNumber"
+          id="cardNumber"
+          value={cardNumber}
+          placeholder="xxxx xxxx xxxx xxxx"
+          onChange={(e) => {
+            setCardNumber(e.target.value);
+            setHighlightCard(Payment.fns.cardType(e.target.value));
+          }}
+        />
+        <label htmlFor="cardHolder">Card Holder</label>
+        <input
+          ref={cardHolderRef}
+          type="text"
+          name="cardHolder"
+          id="cardHolder"
+          value={cardHolder}
+          placeholder="John Deo Shearer"
+          onChange={(e) => setCardHolder(e.target.value)}
+        />
+        <div className="input-horizontal">
+          <div className="input-horizontal--item">
+            <label htmlFor="expirationDate">Exp. Date</label>
+            <input
+              ref={expirationDateRef}
+              className="horizontal"
+              name="expirationDate"
+              id="expirationDate"
+              value={expirationDate}
+              placeholder="MM / YY"
+              onChange={(e) => setExpirationDate(e.target.value)}
+            />
+          </div>
+          <div className="input-horizontal--item">
+            <label htmlFor="cvc">CVC</label>
+            <input
+              ref={cvcRef}
+              value={cvc}
+              className="horizontal"
+              name="cvc"
+              id="cvc"
+              placeholder="xxx"
+              onChange={(e) => setCvc(e.target.value)}
+            />
+          </div>
         </div>
-        <div className="input-horizontal--item">
-          <label htmlFor="cvc">CVC</label>
-          <input
-            ref={cvcRef}
-            value={cvc}
-            className="horizontal"
-            name="cvc"
-            id="cvc"
-            placeholder="xxx"
-            onChange={(e) => setCvc(e.target.value)}
+        <div className="checkout-page-image-container">
+          <img
+            src={visaImage}
+            alt="Visa Card"
+            className={`checkout-page--image ${
+              highlightCard === "visa" ? "select-image" : ""
+            }`}
+          />
+          <img
+            src={mastercardImage}
+            alt="Mastercard"
+            className={`checkout-page--image ${
+              highlightCard === "mastercard" ? "select-image" : ""
+            }`}
+          />
+          <img
+            src={discoverImage}
+            alt="Discover Card"
+            className={`checkout-page--image ${
+              highlightCard === "discover" ? "select-image" : ""
+            }`}
+          />
+          <img
+            src={amexImage}
+            alt="American Express Card"
+            className={`checkout-page--image ${
+              highlightCard === "amex" ? "select-image" : ""
+            }`}
           />
         </div>
       </div>
-    </div>
+      {showLoading ? (
+        <div className="center">
+          <Loader
+            visible={true}
+            type="ThreeDots"
+            color="#795548"
+            width={80}
+            height={80}
+          />
+        </div>
+      ) : (
+        <FormSubmitButton />
+      )}
+    </form>
   );
 };
 
--- client/src/pages/CheckoutPage.js
+++ client/src/pages/CheckoutPage.js
@@ -4,59 +4,24 @@
   Checkout Page
 */
 
-import { useEffect, useRef, useState } from "react";
-import Payment from "payment";
+import { useParams } from "react-router-dom";
+import { motion } from "framer-motion";
+
 import "../css/checkoutPage.css";
 import CardForm from "../components/CardForm";
 
 const CheckoutPage = () => {
-  // Card variables and state
-  const [cardNumber, setCardNumber] = useState("");
-  const [cardHolder, setCardHolder] = useState("");
-  const [expirationDate, setExpirationDate] = useState("");
-  const [cvc, setCvc] = useState("");
-
-  const cardNumberRef = useRef();
-  const expirationDateRef = useRef();
-  const cvcRef = useRef();
-
-  useEffect(() => {
-    Payment.formatCardNumber(cardNumberRef.current);
-    Payment.formatCardCVC(cvcRef.current);
-    Payment.formatCardExpiry(expirationDateRef.current);
-  }, []);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
+  const { mode } = useParams();
+  console.log(mode);
   return (
-    <div className="checkout-page">
-      <form
-        className="checkout-page-form"
-        method="POST"
-        onSubmit={handleSubmit}
-      >
-        <CardForm
-          cardNumber={cardNumber}
-          setCardNumber={setCardNumber}
-          cardHolder={cardHolder}
-          setCardHolder={setCardHolder}
-          expirationDate={expirationDate}
-          setExpirationDate={setExpirationDate}
-          cvc={cvc}
-          setCvc={setCvc}
-          cardNumberRef={cardNumberRef}
-          expirationDateRef={expirationDateRef}
-          cvcRef={cvcRef}
-        />
-        <div className="btn-container">
-          <button className="btn btn-convert" type="submit">
-            <span className="btn-text">Submit</span>
-          </button>
-        </div>
-      </form>
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.7 }}
+      className="checkout-page"
+    >
+      {mode === "Card" && <CardForm />}
+    </motion.div>
   );
 };
 
